68a1d8371985badababc3cbc44f73d1981f45a7c
echenran <echenran@nyu.edu>
2016-02-28 17:09:00 -0500

    things are working

--- index.php
+++ index.php
@@ -5,17 +5,19 @@ require('vendor/twilio/sdk/Services/Twilio.php');
 //$sender = $_REQUEST["From"];
 $msg = explode(" ", $_REQUEST["Body"], 3);
 $sendto = $msg[0];
-$service = $msg[1];
+$service = strtolower($msg[1]);
 $query = $msg[2];
 
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
 
-if (strtolower($service) == "twitter") {
+if ($service) == "twitter") {
 	include 'twitter.php';
-} else if (strtolower($service) == "nytimes") {
+} else if ($service) == "nytimes") {
 	include 'nytimes.php';
+} else if ($service) == "youtube") {
+	include 'youtube.php';
 } else {
 		$client->account->messages->create(array(
 			'To' => $sendto,
