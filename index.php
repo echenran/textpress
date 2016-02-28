b13261e059e2b0061eb1b76aabe4b06e00bf7f71
echenran <echenran@nyu.edu>
2016-02-28 15:15:23 -0500

    ldsafkl

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 require('vendor/twilio/sdk/Services/Twilio.php'); 
 
 $sender = $_REQUEST["From"];
+msg($sendto, $service, $query) = split(" ", $_REQUEST["Body"]), 3)
 
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
 
-echo '<pre>'; print_r($_REQUEST["From"]); echo '</pre>';
-print $_REQUEST["From"];
+//echo '<pre>'; print_r($_REQUEST["From"]); echo '</pre>';
+//print $_REQUEST["From"];
 
 $client->account->messages->create(array( 
 	'To' => "+16465042544", 
