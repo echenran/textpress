fede09a41b6fba4c0f2bcd6b33909d7387c14223
Sahith Nallapareddy <sahith.reddy@gmail.com>
2016-02-28 16:10:30 -0500

    php

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ msg($sendto, $service, $query) = split(" ", $_REQUEST["Body"]), 3)
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
-
+/*
 $client->account->messages->create(array(
 	'To' => $sender,
 	'From' => "+16463744020",
 	'Body' => $query,
 	));
+*/
+if (strtolower($service) == "twitter") {
+	include 'twitter.php';
+} else if (strtolower($service) == "nytimes") {
+	include 'nytimes.php';
+}
 ?>
