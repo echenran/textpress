9f7130e23c0e49f6259bc454171598b3dbb2701b
Sahith Nallapareddy <sahith.reddy@gmail.com>
2016-02-28 15:05:43 -0500

    fixing twitter

--- twitter.php
+++ twitter.php
@@ -21,6 +21,7 @@ $string = json_decode($twitter->setGetfield($getfield)
 	->buildOauth($url, $requestMethod)
 	->performRequest(),$assoc = TRUE);
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+echo '<pre>'; print_r($string); echo '</pre>';
 foreach($string as $items)
 {
 	echo "Time and Date of Tweet: ".$items['created_at']."<br />";
