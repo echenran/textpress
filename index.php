d04a0d122bdeb0463204a252d800b24c363fe529
Sahith Nallapareddy <sahith.reddy@gmail.com>
2016-02-28 15:02:37 -0500

    fixing twitter

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ require('vendor/twilio/sdk/Services/Twilio.php');
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
-
-echo '<pre>'; print_r($_REQUEST["From"]); echo '</pre>';
-print $_REQUEST["From"];
+$body = $_REQUEST["Body"];
 
 $client->account->messages->create(array( 
 	'To' => "+15083530126", 
--- twitter.php
+++ twitter.php
@@ -31,15 +31,4 @@ foreach($string as $items)
 	echo "Followers: ". $items['user']['followers_count']."<br />";
 	echo "Friends: ". $items['user']['friends_count']."<br />";
 	echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-}
-/*
-$account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
-$auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
-$client = new Services_Twilio($account_sid, $auth_token); 
-
-$client->account->messages->create(array( 
-	'To' => "+16465042544", 
-	'From' => "+16463744020", 
-	'Body' => "Hey Jenny! Good luck on the bar exam!", 
-	));
-*/
+}?>
