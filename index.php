5823cbb151d109b75ca123706b19d3db518bd9f2
echenran <echenran@nyu.edu>
2016-02-28 11:34:55 -0500

    changed path to Twilio library

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // this line loads the library 
-require('/path/to/twilio-php/Services/Twilio.php'); 
+require('vendor/twilio/sdk/Services/Twilio.php'); 
  
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
