745cedaf62eee9ba15730f1e915a4417cbb136d6
Sahith Nallapareddy <sahith.reddy@gmail.com>
2016-02-28 14:04:49 -0500

    nytimes api

--- nytimes.php
+++ nytimes.php
@@ -15,10 +15,7 @@ $decoded = json_decode($curl_response, true);
 if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
     die('error occured: ' . $decoded->response->errormessage);
 }
-echo "decoded: ".$decoded;
-foreach($decoded as $items)
-{
-	echo "headline: ".$items['docs']['headline']."<br />";
-}
+echo '<pre>'; print_r($decoded); echo '</pre>';
+
 
 ?>
