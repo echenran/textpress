79a32afd01431a044f93a7c3384fd2e2c83ecb5f
echenran <echenran@nyu.edu>
2016-02-28 14:29:14 -0500

    testing

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 // this line loads the library 
 require('vendor/twilio/sdk/Services/Twilio.php'); 
 
+$sender = $_POST->From;
+$msg = $_POST->Body;
+
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
@@ -10,6 +13,6 @@ $client = new Services_Twilio($account_sid, $auth_token);
 $client->account->messages->create(array( 
 	'To' => "+16465042544", 
 	'From' => "+16463744020", 
-	'Body' => "Pls work", 
+	'Body' => $msg, 
 	));
 ?>
