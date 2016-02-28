d33f75f42324cae4bf769fe255481613e67447a0
echenran <echenran@nyu.edu>
2016-02-28 16:03:55 -0500

    dsafkh

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 //header("Access-Control-Allow-Origin: http://localhost:9000");
+include 'nytimes.php';
+include 'twitter.php';
 
 // this line loads the library 
 require('vendor/twilio/sdk/Services/Twilio.php'); 
 
 $sender = $_REQUEST["From"];
-msg($sendto, $service, $query) = split(" ", $_REQUEST["Body"]), 3)
+msg($sendto, $service, $query) = split(" ", $_REQUEST["Body"]), 3);
 
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
