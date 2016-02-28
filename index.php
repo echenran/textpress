30dcbf91510d7b70f6d9d04883f054572da28711
echenran <echenran@nyu.edu>
2016-02-28 15:45:19 -0500

    adskjfh

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75';
 $auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
 
-$client->account->messages->create(array( 
-	'To' => "+16465042544", 
-	'From' => "+16463744020", 
-	'Body' => $_REQUEST["From"], 
+$client->account->messages->create(array(
+	'To' => "+16465042544",
+	'From' => "+16463744020",
+	'Body' => "dsaf"/*$_REQUEST["From"]*/,
 	));
 ?>
