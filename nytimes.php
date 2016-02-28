8bdcba66d76648d4d2b1800bec9831f9072c11c7
Sahith Nallapareddy <sahith.reddy@gmail.com>
2016-02-28 15:45:36 -0500

    fixing php

--- nytimes.php
+++ nytimes.php
@@ -22,6 +22,9 @@ if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') 
 echo '<pre>'; print_r($decoded); echo '</pre>';
 $items = $decoded[response][docs];
 $toField = "+15083530126";
+$account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
+$auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
+$client = new Services_Twilio($account_sid, $auth_token); 
 $client->account->messages->create(array( 
 	'To' => $toField, 
 	'From' => "+16463744020", 
