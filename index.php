cb956b0e300fad1292ec230c25817273b7fd05e6
echenran <echenran@nyu.edu>
2016-02-28 11:25:15 -0500

    changed SMS response

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-	//for number +16463744020 the URL is http://localhost:8080/receiver.php
-	header("content-type: text/xml");
-		echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-	error_log("hello, this is a test!");		
-?>
-<Response>
-	<Message>Hello, Mobile Monkey</Message>
-</Response>
 
+// this line loads the library 
+require('/path/to/twilio-php/Services/Twilio.php'); 
+ 
+$account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
+$auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
+$client = new Services_Twilio($account_sid, $auth_token); 
+ 
+$client->account->messages->create(array( 
+    'To' => "+16465042544", 
+    'From' => "+16463744020", 
+    'Body' => "Hey Jenny! Good luck on the bar exam!", 
+));
