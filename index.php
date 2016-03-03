d148a13dc1f6618a11645dd6027f8749650dcf2d
echenran <echenran@nyu.edu>
2016-03-02 23:07:03 -0500

    revert

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
 
-//loads the library
-require('vendor/twilio/sdk/Services/Twilio.php'); 
-
 // scrapes the SMS and splits into sender, service, and query
 $msg = explode(" ", $_REQUEST["Body"], 3);
 $sendto = "+1".$msg[0];
 $service = strtolower($msg[1]);
 $query = $msg[2];
 $query = str_replace(" ", "%20", $query);
-$sender = $_REQUEST["From"];
-
-//sets up auth token
-$account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
-$auth_token = 'b0d3d3844073b78843bad5647831cdb7'; 
-$client = new Services_Twilio($account_sid, $auth_token);
 
 if ($service == "twitter") {
 	include 'twitter.php';
@@ -22,12 +13,5 @@ if ($service == "twitter") {
 	include 'nytimes.php';
 } else if ($service == "youtube") {
 	include 'youtube.php';
-} else {
-		//sends return text to sender and alerts of invalid form
-		$client->account->messages->create(array(                                          
-		  'To' => $sender,
-			'From' => "+16463744020",
-			'Body' => "Invalid request. Try again!",
-		));
 }
 ?>
