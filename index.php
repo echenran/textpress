6eb5bd09aef935a6bda008198283e4a63962f292
echenran <echenran@nyu.edu>
2016-03-02 23:01:53 -0500

    debugging

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $sendto = "+1".$msg[0];
 $service = strtolower($msg[1]);
 $query = $msg[2];
 $query = str_replace(" ", "%20", $query);
+$sender = $_REQUEST["From"];
 
 //sets up auth token
 $account_sid = 'AC5ddfda7909b9b25c06d3dbdc2dbe5a75'; 
@@ -23,8 +24,8 @@ if ($service == "twitter") {
 	include 'youtube.php';
 } else {
 		//sends return text to sender and alerts of invalid form
-		$client->account->messages->create(array(
-			'To' => $_REQUEST["From"],
+		$client->account->messages->create(array(                                          
+		  'To' => $sender,
 			'From' => "+16463744020",
 			'Body' => "Invalid request. Try again!",
 		));
